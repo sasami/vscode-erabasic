54eef0635b13c5ff15895e92b13970bbe503d778
sasami <sasami@users.noreply.github.com>
2017-11-05 09:00:42 +0900

    WorkspaceSymbolProvider の Cache 機構を分離して整理
    createFileSystemWatcher の rootPath 指定を削除。(Multi-root Workspaces が導入された際に問題になる可能性がある)
    表示に影響しないので列挙時のファイル名のソートを削除。

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,21 @@
 import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as iconv from 'iconv-lite';
 
 import {
     ExtensionContext, Disposable, CancellationToken,
     CompletionItemProvider, CompletionContext, CompletionItem,
     DocumentSymbolProvider, WorkspaceSymbolProvider, SymbolInformation,
-    TextDocument, Uri, Position,
+    TextDocument, Position,
 } from 'vscode';
 
 import { BuiltinComplationItems } from './completion';
-import { readSymbolInformations } from './symbol';
+import { SymbolInformationRepository, readSymbolInformations } from './symbol';
 
 export function activate(context: ExtensionContext) {
-    let workspaceSymbolProvider = new EraBasicWorkspaceSymbolProvider();
+    let repo = new SymbolInformationRepository();
     context.subscriptions.push(vscode.languages.registerCompletionItemProvider({ language: 'erabasic' }, new EraBasicCompletionItemProvider()));
     context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider({ language: 'erabasic' }, new EraBasicDocumentSymbolProvider()));
-    context.subscriptions.push(vscode.languages.registerWorkspaceSymbolProvider(workspaceSymbolProvider));
-    context.subscriptions.push(workspaceSymbolProvider);
+    context.subscriptions.push(vscode.languages.registerWorkspaceSymbolProvider(new EraBasicWorkspaceSymbolProvider(repo)));
+    context.subscriptions.push(repo);
 }
 
 export function deactivate() {
@@ -37,40 +35,21 @@ class EraBasicDocumentSymbolProvider implements DocumentSymbolProvider {
 }
 
 class EraBasicWorkspaceSymbolProvider implements WorkspaceSymbolProvider {
-    private cache: Map<string, SymbolInformation[]> = new Map();
-    private dirty: Set<string> = new Set();
-
-    private disposable: Disposable;
-
-    constructor() {
-        let watcher = vscode.workspace.createFileSystemWatcher(`${vscode.workspace.rootPath}/**/*.[Ee][Rr][BbHh]`);
-        watcher.onDidCreate((e) => { this.dirty.add(e.fsPath) });
-        watcher.onDidChange((e) => { this.dirty.add(e.fsPath) });
-        watcher.onDidDelete((e) => { this.dirty.delete(e.fsPath); this.cache.delete(e.fsPath) });
-        this.disposable = watcher;
+    constructor(private repo: SymbolInformationRepository) {
     }
 
     provideWorkspaceSymbols(query: string, token: CancellationToken): Promise<SymbolInformation[]> {
-        return this.sync().then(() => {
+        return this.repo.sync().then(() => {
             let pattern = this.compileQuery(query);
-            if (pattern === null) {
-                return [];
+            if (pattern !== undefined) {
+                return this.repo.filter((s) => pattern.test(s.name));
             }
-            let matches: SymbolInformation[] = [];
-            for (let path of Array.from(this.cache.keys()).sort()) {
-                matches.push(...this.cache.get(path).filter((s) => pattern.test(s.name)));
-            }
-            return matches;
         });
-    }
-
-    dispose() {
-        this.disposable.dispose();
     }
 
     private compileQuery(query: string): RegExp {
         if (query.length === 0) {
-            return null;
+            return;
         }
         let chars = Array.from(query).map((c) => {
             let uc = c.toUpperCase();
@@ -78,35 +57,5 @@ class EraBasicWorkspaceSymbolProvider implements WorkspaceSymbolProvider {
             return uc === lc ? c : `[${uc}${lc}]`;
         });
         return new RegExp(chars.join('.*'));
-    }
-
-    private async sync(): Promise<void> {
-        if (this.cache.size === 0) {
-            for (let uri of await vscode.workspace.findFiles('**/*.[Ee][Rr][BbHh]')) {
-                this.dirty.add(uri.fsPath);
-            }
-        }
-        if (this.dirty.size === 0) {
-            return;
-        }
-        let encoding = vscode.workspace.getConfiguration('files').get('encoding', 'utf8');
-        for (let path of Array.from(this.dirty)) {
-            let input = await new Promise<string>((resolve, reject) => {
-                fs.readFile(path, (err, data) => {
-                    if (err) {
-                        if (err.code === 'ENOENT') {
-                            this.dirty.delete(path);
-                            this.cache.delete(path);
-                        }
-                        reject(err);
-                    } else {
-                        resolve(iconv.decode(data, encoding));
-                    }
-                });
-            });
-            if (this.dirty.delete(path)) {
-                this.cache.set(path, readSymbolInformations(Uri.file(path), input));
-            }
-        }
     }
 }
--- src/symbol.ts
+++ src/symbol.ts
@@ -1,4 +1,8 @@
-import { SymbolInformation, SymbolKind, Location, Uri, Range, Position } from 'vscode';
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as iconv from 'iconv-lite';
+
+import { Disposable, SymbolInformation, SymbolKind, Location, Uri, Range, Position } from 'vscode';
 
 function* iterlines(input: string): IterableIterator<[number, string]> {
     let lines = input.split(/\r?\n/);
@@ -55,8 +59,8 @@ export function readSymbolInformations(uri: Uri, input: string): SymbolInformati
                     match[1],
                     SymbolKind.Variable,
                     funcStartName,
-                    new Location(uri, new Range(line, 0, line, text.length)))
-                );
+                    new Location(uri, new Range(line, 0, line, text.length))
+                ));
                 continue;
             }
         }
@@ -68,4 +72,61 @@ export function readSymbolInformations(uri: Uri, input: string): SymbolInformati
         );
     }
     return symbols;
+}
+
+export class SymbolInformationRepository implements Disposable {
+    private cache: Map<string, SymbolInformation[]> = new Map();
+    private dirty: Set<string> = new Set();
+
+    private disposable: Disposable;
+
+    constructor() {
+        let watcher = vscode.workspace.createFileSystemWatcher(`**/*.[Ee][Rr][BbHh]`);
+        watcher.onDidCreate((e) => { this.dirty.add(e.fsPath) });
+        watcher.onDidChange((e) => { this.dirty.add(e.fsPath) });
+        watcher.onDidDelete((e) => { this.dirty.delete(e.fsPath); this.cache.delete(e.fsPath) });
+        this.disposable = watcher;
+    }
+
+    async sync(): Promise<void> {
+        if (this.cache.size === 0) {
+            for (let uri of await vscode.workspace.findFiles('**/*.[Ee][Rr][BbHh]')) {
+                this.dirty.add(uri.fsPath);
+            }
+        }
+        if (this.dirty.size === 0) {
+            return;
+        }
+        let encoding = vscode.workspace.getConfiguration('files').get('encoding', 'utf8');
+        for (let path of Array.from(this.dirty)) {
+            let input = await new Promise<string>((resolve, reject) => {
+                fs.readFile(path, (err, data) => {
+                    if (err) {
+                        if (err.code === 'ENOENT') {
+                            this.dirty.delete(path);
+                            this.cache.delete(path);
+                        }
+                        reject(err);
+                    } else {
+                        resolve(iconv.decode(data, encoding));
+                    }
+                });
+            });
+            if (this.dirty.delete(path)) {
+                this.cache.set(path, readSymbolInformations(Uri.file(path), input));
+            }
+        }
+    }
+
+    filter(cb: (s: SymbolInformation) => any): SymbolInformation[] {
+        let matches: SymbolInformation[] = [];
+        for (let symbols of this.cache.values()) {
+            matches.push(...symbols.filter(cb));
+        }
+        return matches;
+    }
+
+    dispose() {
+        this.disposable.dispose();
+    }
 }
